b687f8b9c51a9cc27b6edbc3f66aa2d9907b22e6
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-27 14:56:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,14 @@ from tensorflow.keras.models import load_model
 model_load = load_model('model')
 
 st.title('CIFAR100 Image Recognizer')
-labels = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+labels = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 
+          'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 
+          'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard',
+          'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 
+          'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket',
+          'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 
+          'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf',
+          'woman', 'worm']
 
 
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
